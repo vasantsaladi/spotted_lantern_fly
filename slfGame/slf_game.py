233d5536e2104c56aa9b14cc2c51e265908aeed8
cyber-rose <66093258+cyber-rose@users.noreply.github.com>
2024-10-05 18:52:08 -0400

    Background for squash level

--- slfGame/slf_game.py
+++ slfGame/slf_game.py
@@ -1,4 +1,9 @@
 import pygame
+import pygame_menu
+from random import randint
+from pathlib import Path
+from typing import Tuple
+
 
 # Import pygame.locals for easier access to key coordinates
 # Updated to conform to flake8 and black standards
@@ -12,12 +17,12 @@ from pygame.locals import (
     QUIT,
 )
 
-from random import randint
-from pathlib import Path
-from typing import Tuple
+
 
 # pygame setup
 pygame.init()
+
+# Define the stages of the game
 
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
@@ -27,23 +32,25 @@ slf_countdown = 2500
 slf_interval = 100
 
 # Amount of SLFs allowed on screen
-SLF_COUNT = 10
+SLF_COUNT = 20
 
+# Spotted Lanternfly class
 class SpottedLanFly(pygame.sprite.Sprite):
     def __init__(self):
         super(SpottedLanFly, self).__init__()
 
-        #get image to draw for lanternfly
+        #get image path to draw for lanternfly
         slf_image = str(Path.cwd() / "slfGame" / "resources" / "images" / "slf_sprite.png")
 
         # Load the image, preserve alpha channel for transparency
         self.surf = pygame.image.load(slf_image).convert_alpha()
+        sizeMod = randint(-100, 500) # Variable to modify the size of the Image
+        self.surf = pygame.transform.scale(self.surf, (self.surf.get_width() - sizeMod, self.surf.get_height() - sizeMod))
 
         # Save the rect so you can move it
         self.rect = self.surf.get_rect()
 
-        # The starting position is randomly generated
-
+        # Starting position
         self.rect = self.surf.get_rect(
             center=(
                 randint(10, SCREEN_WIDTH - 10),
@@ -52,10 +59,43 @@ class SpottedLanFly(pygame.sprite.Sprite):
         )
         
 
+# Render background for the Squash Level
+def render_squash_background():
+    screen.fill((83, 205, 255))
+
+    #draw the ground
+    pygame.draw.rect(screen, (36, 113, 20), pygame.Rect(0, SCREEN_HEIGHT/2, SCREEN_WIDTH, SCREEN_HEIGHT/2))
+
+    #insert the trees
+    tree_img = pygame.image.load(str(Path.cwd() / "slfGame" / "resources" / "images" / "tree-3707718_1280.png")).convert_alpha()
+
+    tree_img = pygame.transform.scale(tree_img, (tree_img.get_width()/2, tree_img.get_height()/2))
+
+    #place first tree
+    screen.blit(tree_img, (0, SCREEN_HEIGHT - tree_img.get_height()))
+
+    #place second tree
+    screen.blit(tree_img, (SCREEN_WIDTH - tree_img.get_width(), SCREEN_HEIGHT - tree_img.get_height()))
+
+
 # Create the screen object
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Spotted Lanternfly Squash")
 clock = pygame.time.Clock()
 
+'''
+def start_game():
+    mainmenu._open(level)
+
+mainmenu = pygame_menu.Menu('Welcome', 600, 400, theme= pygame_menu.themes.THEME_SOLARIZED)
+mainmenu.add.text_input('Name: ', default='username', maxchar=20)
+mainmenu.add.button('Play', start_game)
+#mainmenu.add.button('Levels', level_menu)
+mainmenu.add.button('Quit', pygame_menu.events.EXIT)
+
+level = pygame_menu.Menu('Select a Difficulty', 600, 400, theme=pygame_menu.themes.THEME_BLUE)
+level.add.selector('Difficulty :', [('Hard', 1), ('Easy', 2)], onchange=set_difficulty)
+'''
 # a custom event to add a new SLF
 ADDSLF = pygame.USEREVENT + 1
 pygame.time.set_timer(ADDSLF, slf_countdown)
@@ -67,10 +107,13 @@ score = 0
 running = True
 while running:
     # poll for events
-    for event in pygame.event.get():
+    events = pygame.event.get()
+    for event in events:
         # Player closed the game
         if event.type == pygame.QUIT:
             running = False
+
+
         elif event.type == ADDSLF:
             # Create a new moth and add it to the coin list
             new_slf = SpottedLanFly()
@@ -83,18 +126,17 @@ while running:
                 slf_countdown = 100
 
             pygame.time.set_timer(ADDSLF, 0)
-
             pygame.time.set_timer(ADDSLF, slf_countdown)
+
 
     # RENDER YOUR GAME HERE
 
     # Are there too many coins on the screen?
     if len(slf_list) >= SLF_COUNT:
-        # This counts as an end condition, so you end your game loop
+        # This counts as an end condition, so you end your game loop. It shuts down the game
         running = False
 
-    # To render the screen, first fill the background with a color to wipe away anything from last frame
-    screen.fill("purple")
+    render_squash_background()
 
     # Draw the slf next
     for slf in slf_list:
